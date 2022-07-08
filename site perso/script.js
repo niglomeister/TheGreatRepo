cfa58add53479757a36dd61373929e5c4d90bf30
Maxence <jsp@r.com>
2022-07-08 23:03:27 +0200

    je m'étais trompé de dossier

---
+++ site perso/script.js
@@ -0,0 +1,9 @@
+
+function search(){
+    let query = document.getElementById("query field").value;
+    
+    if (query != "") console.log(`searching for ${query} ...`);
+    else console.log("no query provided");
+
+    
+}
